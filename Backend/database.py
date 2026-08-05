9838e137b0db56bf2501ae4d6ef3ace3cd22bc8e
Haris Waheed <ah6086395@gmail.com>
2026-08-05 14:26:08 +0500

    Formatted Code

--- Backend/database.py
+++ Backend/database.py
@@ -5,15 +5,17 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-URL = f'postgresql://{DB_USER}:{DB_PASS}@ep-orange-lab-azpli8dh-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+URL = f"postgresql://{DB_USER}:{DB_PASS}@ep-orange-lab-azpli8dh-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
 
 async def db_pool():
 
     return await asyncpg.create_pool(  # type: ignore
-        dsn=URL, min_size=2, max_size=11
+        dsn=URL, min_size=2, max_size=11, timeout=60
     )
+
 
 async def get_db(request: Request):
 
@@ -21,7 +23,7 @@ async def get_db(request: Request):
     connection = await pool.acquire()
 
     try:
-        'Database Connected'
+        "Database Connected"
         yield connection
 
     finally:

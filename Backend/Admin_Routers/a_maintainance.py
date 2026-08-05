870e8f9c2dc95f58f80e01305743ee9c1f73530d
Haris Waheed <ah6086395@gmail.com>
2026-08-05 14:24:32 +0500

    Added complain status update for landlord and delete complain for tenant portal

--- Backend/Admin_Routers/a_maintainance.py
+++ Backend/Admin_Routers/a_maintainance.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from models.maintain_model import NewMaintenanceRequest
+from models.maintain_model import NewMaintenanceRequest, MaintenanceStatusUpdate
 from database import get_db
 import asyncpg
 
@@ -10,20 +10,27 @@ router = APIRouter(prefix="/maintain", tags=["ADMIN MAINTENANCE"])
 async def add_maintenance(
     request_data: NewMaintenanceRequest, db: asyncpg.Connection = Depends(get_db)
 ):
-    insert_query = "CALL p_add_maintenance($1, $2, $3)"
+    insert_query = "CALL p_add_maintenance($1, $2, $3, $4, NULL, NULL)"
 
     try:
         async with db.transaction():
-            await db.execute(
+            result = await db.fetch(
                 insert_query,
                 request_data.lease_id,
                 request_data.issue_title,
                 request_data.priority,
+                request_data.description
             )
+
+            response_model = request_data.model_dump()
+
+            for record in result:
+                response_model['request_id'] = record[0]
+                response_model['request_date'] = record[1]
 
             return {
                 "status": "success",
-                "data": request_data.model_dump(),
+                "data": response_model,
                 "message": "Maintenance request submitted successfully!",
             }
 
@@ -39,17 +46,20 @@ async def add_maintenance(
 async def get_all_maintenance(db: asyncpg.Connection = Depends(get_db)):
     try:
         async with db.transaction():
-            await db.execute("CALL p_get_maintenance('maintenance_cursor')")
+            await db.execute("CALL p_get_maintenance('maintenance_cursors')")
 
-            records = await db.fetch("FETCH ALL FROM maintenance_cursor")
+            records = await db.fetch("FETCH ALL FROM maintenance_cursors")
 
             maintenance_list = []
             for record in records:
                 maintenance_item = {
+                    "request_id": record["request_id"],
                     "date": record["request_date"],
                     "issue": record["issue_title"],
                     "location": record["location"],
+                    "unit_assign": record["unit_assign"],
                     "priority": record["priority"],
+                    "description": record["description"],
                     "status": record["status"],
                 }
                 maintenance_list.append(maintenance_item)
@@ -62,3 +72,45 @@ async def get_all_maintenance(db: asyncpg.Connection = Depends(get_db)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to fetch maintenance details",
         )
+
+
+@router.patch("/update_maintenance_status")
+async def update_maintenance_status(
+        payload: MaintenanceStatusUpdate,
+        db: asyncpg.Connection = Depends(get_db)
+):
+    try:
+        async with db.transaction():
+
+            update_query = "CALL p_update_maintenance_status($1, $2)"
+            await db.execute(update_query, payload.request_id, payload.status)
+
+            return {
+                "status": "success",
+                "message": f"Status updated to '{payload.status}' successfully!"
+            }
+
+    except Exception as e:
+        print(f"Database error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@router.delete("/delete_maintenance_request/{request_id}")
+async def delete_maintenance_request(
+        request_id: int,
+        db: asyncpg.Connection = Depends(get_db)
+):
+    try:
+        async with db.transaction():
+
+            delete_query = "CALL p_delete_request($1)"
+            await db.execute(delete_query, request_id)
+
+            return {
+                "status": "success",
+                "message": f"Maintenance request {request_id} deleted successfully!"
+            }
+
+    except Exception as e:
+        print(f"Database error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
---
+++ Backend/DataBase/a_maintenance.sql
@@ -0,0 +1,85 @@
+CREATE TABLE MAINTENANCE_REQUESTS (
+    request_id SERIAL PRIMARY KEY,
+    lease_id INT NOT NULL REFERENCES LEASES(lease_id) ON DELETE CASCADE ,
+    issue_title VARCHAR(255) NOT NULL,
+    description text,
+    priority VARCHAR(50) NOT NULL DEFAULT 'Low',
+    status VARCHAR(50) NOT NULL DEFAULT 'Pending',
+    request_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE OR REPLACE PROCEDURE p_add_maintenance(
+    p_lease_id IN INT,
+    p_issue_title IN VARCHAR,
+    p_priority IN VARCHAR,
+    p_description IN VARCHAR,
+    r_request_id OUT VARCHAR,
+    r_request_date OUT DATE
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO MAINTENANCE_REQUESTS (lease_id, issue_title, priority, description, status, request_date)
+    VALUES (p_lease_id, p_issue_title, p_priority,
+            p_description,'Pending', CURRENT_DATE)
+
+    RETURNING request_id, request_date INTO r_request_id, r_request_date;
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE p_get_maintenance(
+    p_cursor INOUT refcursor
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    OPEN p_cursor FOR
+    SELECT
+        M.request_id,
+        M.request_date,
+        M.issue_title,
+        P.prop_name AS location,
+        L.unit_assign,
+        M.priority,
+        M.description,
+        M.status
+    FROM MAINTENANCE_REQUESTS M
+    LEFT JOIN LEASES L
+        ON M.lease_id = L.lease_id
+    LEFT JOIN PROPERTIES P
+        ON L.prop_id = P.prop_id
+    WHERE L.lease_status = 'Active'
+    ORDER BY M.request_date DESC;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE p_update_maintenance_status(
+    p_request_id IN INT,
+    p_new_status IN VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE maintenance_requests
+    SET status = p_new_status
+    WHERE request_id = p_request_id;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE p_delete_request(
+    p_request_id IN INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+    DELETE FROM MAINTENANCE_REQUESTS
+    WHERE request_id = p_request_id;
+end;
+    $$;
+
+CREATE OR REPLACE PROCEDURE p_get_admin_maintain(
+
+    cursror
+)

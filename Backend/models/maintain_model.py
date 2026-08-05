980b7a70f9b7b716d405738c320fab57e5e16496
Haris Waheed <ah6086395@gmail.com>
2026-08-05 14:36:13 +0500

    Added status update model

--- Backend/models/maintain_model.py
+++ Backend/models/maintain_model.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
+from typing import Literal, Optional
 
 class NewMaintenanceRequest(BaseModel):
     lease_id: int
-    issue_title: str
-    priority: str
+    issue_title: str = Field(..., max_length=255)
+    description: Optional[str] = None
+    priority: Literal["Low", "High"] = Field(...)
+
+
+class MaintenanceStatusUpdate(BaseModel):
+    request_id: int
+    status: Literal['Pending', 'In Progress', 'Resolved']
